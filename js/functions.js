e1d5c347604d16596eaf9522ea8604b08a0c9bfb
Benyamin Ghanbari <121010832+Benyamin-Ghanbari@users.noreply.github.com>
2024-04-21 19:51:48 +0330

    Update functions.js

--- js/functions.js
+++ js/functions.js
@@ -1,34 +1,25 @@
-
 var grid_animating;
 
 $(window).keypress(function (e) {
-	if (e.which == 103)
-	{
-	
-	if(!grid_animating)
-	{
-		grid_animating = true;
-	
-		if($("body.grid").length > 0)
-		{
-			$("body").removeClass("grid");
-		}
-		else {
-			$("body").addClass("grid");
-		}
-	}
-	}
+    if (e.which == 103) {
+        if (!grid_animating) {
+            grid_animating = true;
+            $("body").toggleClass("grid");
+            if ($("body").hasClass("grid")) {
+                $("body").css("background-color", "#007bff"); // or any other shade of blue you prefer
+            } else {
+                $("body").css("background-color", ""); // Reset to default background color if needed
+            }
+        }
+    }
 });
 
 function checkWindowSize() {
-	
-	if ( $(window).width() < 950 ) {
-		$('body').addClass('small');
-	}
-	else {
-		$('body').removeClass('small');
-	}
-	
+    if ($(window).width() < 950) {
+        $('body').addClass('small');
+    } else {
+        $('body').removeClass('small');
+    }
 }
 
 $(window).resize(checkWindowSize);
