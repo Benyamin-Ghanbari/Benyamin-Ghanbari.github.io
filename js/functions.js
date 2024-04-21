d9df8cefce0d599c4f4537a2afebc1c0071bf139
Benyamin Ghanbari <121010832+Benyamin-Ghanbari@users.noreply.github.com>
2024-04-21 20:00:27 +0330

    Update functions.js

--- js/functions.js
+++ js/functions.js
@@ -1,18 +1,34 @@
+
+var grid_animating;
+
 $(window).keypress(function (e) {
-    if (e.which == 103) {
-        $("body").css({
-            "background-image": "none", // Remove background image
-            "background-color": "#c4e1ff" // Set background color to blue
-        });
-    }
+	if (e.which == 103)
+	{
+	
+	if(!grid_animating)
+	{
+		grid_animating = true;
+	
+		if($("body.grid").length > 0)
+		{
+			$("body").removeClass("grid");
+		}
+		else {
+			$("body").addClass("grid");
+		}
+	}
+	}
 });
 
 function checkWindowSize() {
-    if ($(window).width() < 950) {
-        $('body').addClass('small');
-    } else {
-        $('body').removeClass('small');
-    }
+	
+	if ( $(window).width() < 950 ) {
+		$('body').addClass('small');
+	}
+	else {
+		$('body').removeClass('small');
+	}
+	
 }
 
 $(window).resize(checkWindowSize);
