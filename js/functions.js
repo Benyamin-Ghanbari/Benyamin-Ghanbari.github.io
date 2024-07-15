d7729027d36031f04ce8492f5cd572a4882bf786
Benyamin Ghanbari <youngbenyamiin@gmail.com>
2024-07-15 13:15:29 +0330

    Update

--- js/functions.js
+++ js/functions.js
@@ -1,35 +1,8 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const dynamicText = document.getElementById('dynamic-text');
+    const text = "Hey There";
+    let index = 0;
 
-var grid_animating;
-
-$(window).keypress(function (e) {
-	if (e.which == 103)
-	{
-	
-	if(!grid_animating)
-	{
-		grid_animating = true;
-	
-		if($("body.grid").length > 0)
-		{
-			$("body").removeClass("grid");
-		}
-		else {
-			$("body").addClass("grid");
-		}
-	}
-	}
-});
-
-function checkWindowSize() {
-	
-	if ( $(window).width() < 950 ) {
-		$('body').addClass('small');
-	}
-	else {
-		$('body').removeClass('small');
-	}
-	
-}
-
-$(window).resize(checkWindowSize);
-$(window).load(checkWindowSize);
+    function typeText() {
+        if (index < text.length) {
+            dynamicText.innerText += text.charAt(index);
